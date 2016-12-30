7ce419b1426df985584b9e4ed61d2f5e5fdb9b7b
grithope <zhunti99999@gmail.com>
2016-12-30 12:45:13 +0800

    when created a group, join it automatically

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :groups do
-    resources :posts
-  end
+
+        member do
+          post :join
+          post :quit
+        end
+
+        resources :posts
+      end
+
+
   root 'groups#index'
 end
